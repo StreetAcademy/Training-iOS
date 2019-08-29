8ed2cd1057433d2a34ed0d01bc5b6baa1d225f3c
fuwamaki <yusaku.maki@street-academy.com>
2019-08-29 19:39:11 +0900

    add Training-iOS Project

---
+++ Training-iOS/Training-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Training-iOS
+//
+//  Created by yusaku maki on 2019/08/29.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
