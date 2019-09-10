43425d89a9b03832334cf7b8b832db03fa145220
Shouta Taruawa <shouta859@gmail.com>
2019-09-10 17:04:52 +0900

    セルタップ時に画面遷移完了

--- Training-iOS/TableViewController.swift
+++ Training-iOS/TableViewController.swift
@@ -8,15 +8,41 @@
 
 import UIKit
 
-class TableViewController: UIViewController {
+class TableViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    let list = ["研修1","研修2","研修3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
     
-
- 
-
+    @IBOutlet var tableView: UITableView!
+    //セルの個数を指定するデリゲートメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+        
+    }
+    
+    
+    //セルに値を設定するデータソースメソッド
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        // セルに表示する値を設定する
+        cell.textLabel!.text = list[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        let ViewController = self.storyboard?.instantiateViewController(withIdentifier: "NextView") as! ViewController
+        
+        self.navigationController?.pushViewController(ViewController, animated: true)
+    }
+    
 }
