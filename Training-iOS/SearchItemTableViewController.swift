92c897daab20e8030e244ab557aed926b460e388
tarukawa <shouta859@gmail.com>
2019-09-18 18:07:40 +0900

    研修用App　画面と処理を実装しなおし
    api 参考になりそうな本を見つけたため

---
+++ Training-iOS/SearchItemTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchItemTableViewController.swift
+//  Training-iOS
+//
+//  Created by Shouta Tarukawa on 2019/09/18.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+class SearchItemTableViewController: UITableViewController,UISearchBarDelegate {
+    var itemDataArray = [ItemData]() // 検索した商品情報の1つ1つのデータを格納するための配列
+    
+    
+}
