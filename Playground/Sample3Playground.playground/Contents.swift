5da1fccc7e69532e4f6e92865a16247660be71e8
fuwamaki <yusaku.maki@street-academy.com>
2019-09-24 17:51:47 +0900

    class and struct

--- Playground/Sample3Playground.playground/Contents.swift
+++ Playground/Sample3Playground.playground/Contents.swift
@@ -47,3 +47,68 @@ print(mori.count)
 
 let kobayashi = Apple(count: 10, name: "takeshi")
 print(mori.name)
+
+
+// struct
+struct Banana {
+    var count: Int
+
+    init(count: Int) {
+        self.count = count
+    }
+}
+
+let hayashi = Banana(count: 4)
+
+// classとstructの違い1
+
+struct test1 {
+    let cat: String
+    let dog: String
+}
+
+class test2 {
+    let cat: String
+    let dog: String
+    init(cat: String, dog: String) {
+        self.cat = cat
+        self.dog = dog
+    }
+}
+
+var a = test1(cat: "たま", dog: "ぽち")
+let b = test1(cat: "たま2", dog: "ぽち2")
+
+print(a.cat)
+print(b.cat)
+
+// classとstructの違い2
+
+class cat {
+    var like: String = "魚"
+}
+
+struct dog {
+    var like: String = "肉"
+}
+
+let tama: cat = cat()
+let pochi: dog = dog()
+
+func initWithData(_ data: cat) {
+ var data = data
+ data.like = "マタタビ"
+}
+
+func initWithData2(_ data: dog) {
+ var data = data
+ data.like = "マタタビ"
+}
+
+initWithData(tama)
+initWithData2(pochi)
+
+print(tama.like)
+print(pochi.like)
+// structはinitializeの時にしか、値を入れられない
+// classはいつでも入れられる
