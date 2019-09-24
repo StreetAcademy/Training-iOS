bb2855a4e87f5c6110d12623053f0c266a86a64a
tarukawa <shouta859@gmail.com>
2019-09-24 15:17:02 +0900

    コメントアウト解除

--- Playground/TestGround.playground/Contents.swift
+++ Playground/TestGround.playground/Contents.swift
@@ -1,26 +1,26 @@
 //import UIKit
 //
 //
-//// if文で 1 ~ 100まで表示 3の倍数と3がつく時バカを表示
-//for i in  1...100 {
-//    if i % 3 == 0 || i % 10 == 3 || i >= 30 && i < 40 {
-//        print("バカ")
-//    }
-//    else{
-//        print(i)
-//    }
-//}
-//
-//// swicth文で1~100までを表示3の倍数と3がつくときバカを表示
-//// fallthroughは、一つ下の条件式の中（条件式に関係なく）に入る
-//for i in 1...100 {
-//    switch i {
-//    case let num where num % 3 == 0 || num  % 10 == 3 || num >= 30 && num < 40:
-//        print("バカ")
-//    default:
-//        print(i)
-//    }
-//}
+// if文で 1 ~ 100まで表示 3の倍数と3がつく時バカを表示
+for i in  1...100 {
+    if i % 3 == 0 || i % 10 == 3 || i >= 30 && i < 40 {
+        print("バカ")
+    }
+    else{
+        print(i)
+    }
+}
+
+// swicth文で1~100までを表示3の倍数と3がつくときバカを表示
+// fallthroughは、一つ下の条件式の中（条件式に関係なく）に入る
+for i in 1...100 {
+    switch i {
+    case let num where num % 3 == 0 || num  % 10 == 3 || num >= 30 && num < 40:
+        print("バカ")
+    default:
+        print(i)
+    }
+}
 
 // 3 = Buzz, 5 = fizz, 3と5の公倍数Buzz&Fizzを表示
 for i in 1...100 {
