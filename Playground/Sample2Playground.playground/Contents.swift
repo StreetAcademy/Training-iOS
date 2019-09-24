69d97fb6ebdc174773261edcbe8453c69339897b
fuwamaki <yusaku.maki@street-academy.com>
2019-09-24 15:51:35 +0900

    enum

--- Playground/Sample2Playground.playground/Contents.swift
+++ Playground/Sample2Playground.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
 var str = "Hello, playground"
+
+enum JR {
+    case ebisu
+    case shibuya
+    case ikebukuro
+    case mejiro
+    case meguro
+}
+
+let station: JR = .ebisu
+
+let stations: [JR] = [.ebisu, .shibuya, .ikebukuro]
+
+stations.forEach { station in
+    switch station {
+    case .ebisu:
+        print("恵比寿")
+    case .shibuya:
+        print("渋谷")
+    case .ikebukuro:
+        print("池袋")
+    case .mejiro:
+        print("目白")
+    case .meguro:
+        print("目黒")
+    }
+}
