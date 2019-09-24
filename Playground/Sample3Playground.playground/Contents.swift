7bc09fef3648b8558af1470247938debc38599a6
fuwamaki <yusaku.maki@street-academy.com>
2019-09-24 17:21:28 +0900

    class and static

--- Playground/Sample3Playground.playground/Contents.swift
+++ Playground/Sample3Playground.playground/Contents.swift
@@ -1,3 +1,49 @@
 import UIKit
 
-var str = "Hello, playground"
+class Dog {
+
+    var name: String = ""
+    static var type = "犬"
+}
+
+// 複製できものは()付きで参照できる
+let suzuki = Dog()
+suzuki.name = "ポチ"
+print(Dog.type)
+print(suzuki.name)
+
+Dog.type = "猫"
+
+let sato = Dog()
+sato.name = "ハゲ"
+print(Dog.type)
+print(sato.name)
+
+// staticなものは()を付けない
+var tanaka = Dog.type
+tanaka = "イルカ"
+
+print(Dog.type)
+
+// ---
+
+class Apple {
+
+    private(set) var count = 0
+    private(set) var name = ""
+
+    init(count: Int) {
+        self.count = count
+    }
+
+    init(count: Int, name: String) {
+        self.count = count
+        self.name = name
+    }
+}
+
+let mori = Apple(count: 5)
+print(mori.count)
+
+let kobayashi = Apple(count: 10, name: "takeshi")
+print(mori.name)
