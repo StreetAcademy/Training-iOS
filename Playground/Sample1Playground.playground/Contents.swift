26787d7017c9eee1885901cb889c46a9268f45e3
fuwamaki <yusaku.maki@street-academy.com>
2019-09-24 12:04:44 +0900

    if let

--- Playground/Sample1Playground.playground/Contents.swift
+++ Playground/Sample1Playground.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var a = 1
+let b = 1
+
+if a == 1 {
+    print("success")
+} else {
+    print("failure")
+}
+
+let test: String? = "test"
+print(test)
+
+// if letはoptionalのものの、中身を出せる
+// 変数名を同じにできる
+if let test = test {
+    print(test)
+}
