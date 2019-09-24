37093ced09c3b4667d3dca4c5c40739aba768f70
tarukawa <shouta859@gmail.com>
2019-09-24 12:42:29 +0900

    for if文

--- Playground/TestGround.playground/Contents.swift
+++ Playground/TestGround.playground/Contents.swift
@@ -1,9 +1,14 @@
 import UIKit
 
 
-//1 ~ 100まで表示
-for i in  1...100  {
-    print(i)
-}
+//1 ~ 100まで表示 3の倍数の時バカを表示
+for i in  1...100 {
+    if i % 3 == 0 {
+        print("バカ")
+    }
+    else{
+        print(i)
 
+    }
+}
 
