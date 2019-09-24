50913735d5ede8ac8eece0b756fe909789453d76
fuwamaki <yusaku.maki@street-academy.com>
2019-09-24 16:18:52 +0900

    add timeFromUeno

--- Playground/Sample2Playground.playground/Contents.swift
+++ Playground/Sample2Playground.playground/Contents.swift
@@ -8,6 +8,21 @@ enum JR: String {
     case ikebukuro = "池袋"
     case mejiro = "目白"
     case meguro = "目黒"
+
+    var timeFromUeno: Int {
+        switch self {
+        case .ebisu:
+            return 27
+        case .shibuya:
+            return 25
+        case .ikebukuro:
+            return 14
+        case .mejiro:
+            return 15
+        case .meguro:
+            return 30
+        }
+    }
 }
 
 let station: JR = .ebisu
@@ -15,5 +30,5 @@ let station: JR = .ebisu
 let stations: [JR] = [.ebisu, .shibuya, .ikebukuro]
 
 stations.forEach { station in
-    print(station.rawValue)
+    print("\(station.rawValue): \(station.timeFromUeno)")
 }
