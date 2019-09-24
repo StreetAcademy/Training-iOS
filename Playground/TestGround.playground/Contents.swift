d31124eee1e6808e0df4fc23b1cb8dfa7b09eb2a
tarukawa <shouta859@gmail.com>
2019-09-24 15:16:07 +0900

    Fizz Buzz問題

--- Playground/TestGround.playground/Contents.swift
+++ Playground/TestGround.playground/Contents.swift
@@ -10,14 +10,34 @@
 //        print(i)
 //    }
 //}
+//
+//// swicth文で1~100までを表示3の倍数と3がつくときバカを表示
+//// fallthroughは、一つ下の条件式の中（条件式に関係なく）に入る
+//for i in 1...100 {
+//    switch i {
+//    case let num where num % 3 == 0 || num  % 10 == 3 || num >= 30 && num < 40:
+//        print("バカ")
+//    default:
+//        print(i)
+//    }
+//}
 
-// swicth文で1~100までを表示3の倍数と3がつくときバカを表示
-// fallthroughは、一つ下の条件式の中（条件式に関係なく）に入る
+// 3 = Buzz, 5 = fizz, 3と5の公倍数Buzz&Fizzを表示
 for i in 1...100 {
-    switch i {
-    case let num where num % 3 == 0 || num  % 10 == 3 || num >= 30 && num < 40:
-        print("バカ")
-    default:
+    // 3と5の倍数Fizz&Buzz表示
+    if i % 3 == 0 && i % 5 == 0 {
+        print("Fizz&Buzz")
+    }
+    // 3の倍数Buzz
+    else if i % 3 == 0 {
+        print("Buzz")
+    }
+    // 5の倍数 Fizz
+    else if i % 5 == 0 {
+        print("Fizz")
+    }
+    else {
         print(i)
     }
 }
+
