7500d9787f2dbd7c2856ce26e8ac04af33c07460
tarukawa <shouta859@gmail.com>
2019-09-25 19:27:51 +0900

    画面遷移先にnavigationContorollerを配置する方法

--- Training-iOS/TestViewController.swift
+++ Training-iOS/TestViewController.swift
@@ -12,11 +12,12 @@ class TestViewController: UIViewController {
 
     @IBAction func clickButton(_ sender: Any) {
         // ストーリーボードを定義する
-        let storyboard = UIStoryboard(name: "Test1", bundle: nil)
+        let storyboard = UIStoryboard(name: "Test2", bundle: nil)
         
         // 下からpushして画面遷移する
-        if let viewController = storyboard.instantiateViewController(withIdentifier: "TestViewController") as? TestViewController {
-            self.present(viewController, animated: true, completion: nil)
+        if let viewController = storyboard.instantiateViewController(withIdentifier: "Test2ViewController") as? Test2ViewController {
+            let navigationController = UINavigationController(rootViewController: viewController)
+            self.present(navigationController, animated: true, completion: nil)
         }
     }
 
