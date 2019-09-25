d6b6917c77270837c178633ea987065a61747240
tarukawa <shouta859@gmail.com>
2019-09-25 19:17:43 +0900

    画面遷移 presentの方法

--- Training-iOS/TestViewController.swift
+++ Training-iOS/TestViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class TestViewController: UIViewController {
 
+    @IBAction func clickButton(_ sender: Any) {
+        // ストーリーボードを定義する
+        let storyboard = UIStoryboard(name: "Test1", bundle: nil)
+        
+        // 下からpushして画面遷移する
+        if let viewController = storyboard.instantiateViewController(withIdentifier: "TestViewController") as? TestViewController {
+            self.present(viewController, animated: true, completion: nil)
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
