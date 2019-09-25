8e74860c15b4be919933f73475b9de94940d7634
tarukawa <shouta859@gmail.com>
2019-09-25 18:48:21 +0900

    segueを使わない画面遷移

--- Training-iOS/Test2ViewController.swift
+++ Training-iOS/Test2ViewController.swift
@@ -12,10 +12,13 @@ class Test2ViewController: UIViewController {
 
     @IBAction func nextButton(_ sender: Any) {
         print("pushed")
+        let storyboard = UIStoryboard(name: "Test1", bundle: nil)
+        if let viewController = storyboard.instantiateViewController(withIdentifier: "TestViewController") as? TestViewController {
+            self.navigationController?.pushViewController(viewController, animated: true)
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 }
