269ff88ff03e2d2490f777d4e08c0c2fc5437e6f
tarukawa <shouta859@gmail.com>
2019-09-25 17:44:05 +0900

    Create NavigationContlloer

---
+++ Training-iOS/Test2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Test2ViewController.swift
+//  Training-iOS
+//
+//  Created by Shouta Tarukawa on 2019/09/25.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+class Test2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
