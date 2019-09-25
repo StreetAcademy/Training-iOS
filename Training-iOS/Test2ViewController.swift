dc117b12c6b3c833f5ac1781661d836eaf938c19
tarukawa <shouta859@gmail.com>
2019-09-25 19:13:18 +0900

    コメント追加

--- Training-iOS/Test2ViewController.swift
+++ Training-iOS/Test2ViewController.swift
@@ -12,7 +12,11 @@ class Test2ViewController: UIViewController {
 
     @IBAction func nextButton(_ sender: Any) {
         print("pushed")
+        
+        // ストーリーボードを定義する
         let storyboard = UIStoryboard(name: "Test1", bundle: nil)
+        
+        // 横からpushして画面遷移する
         if let viewController = storyboard.instantiateViewController(withIdentifier: "TestViewController") as? TestViewController {
             self.navigationController?.pushViewController(viewController, animated: true)
         }
