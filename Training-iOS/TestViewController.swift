480a8c922f270c40516bdc1fe6013bce8707cc86
tarukawa <shouta859@gmail.com>
2019-09-25 17:20:31 +0900

    storyboardとclassの接続

---
+++ Training-iOS/TestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TestViewController.swift
+//  Training-iOS
+//
+//  Created by Shouta Tarukawa on 2019/09/25.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
