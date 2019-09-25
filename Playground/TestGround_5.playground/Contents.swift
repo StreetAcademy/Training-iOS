0e0eeca701e693edd3799ea49f636534b679e0eb
tarukawa <shouta859@gmail.com>
2019-09-25 16:24:35 +0900

    enumでtableView表示

--- Playground/TestGround_5.playground/Contents.swift
+++ Playground/TestGround_5.playground/Contents.swift
@@ -1,22 +1,74 @@
 //: A UIKit based Playground for presenting user interface
-  
+
 import UIKit
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
-
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
-        self.view = view
+enum Communication: String {
+    case inInfoAcquisition = "情報取得中"
+    case displaying = "表示中"
+    case editing = "編集中"
+    case editingResultSending = "編集結果を送信中"
+    case editingResultSendingComp = "編集結果送信完了"
+    
+    // enumで個別に付随する値を持つ
+    var viewMessage: String {
+        switch self {
+        case .inInfoAcquisition:
+            return "ロード中"
+        case .displaying:
+            return ""
+        case .editing:
+            return "編集中"
+        case .editingResultSending:
+            return "送信中"
+        case .editingResultSendingComp:
+            return "送信完了"
+        }
     }
 }
-// Present the view controller in the Live View window
 
-PlaygroundPage.current.liveView = MyViewController()
+let communications: [Communication] = [.inInfoAcquisition, .displaying, .editing, .editingResultSending, .editingResultSendingComp]
+
+final class FoodTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return communications.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        
+        // セルに表示されるもタイトルを編集（メインタイトル）
+        cell.textLabel?.text = communications[indexPath.row].rawValue
+        // セルに表示されるもタイトルを編集（サブタイトル）
+        cell.detailTextLabel?.text = communications[indexPath.row].viewMessage
+
+        return cell
+    }
+}
+
+// Playgroundでの非同期処理を待つオプション
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+// 画面の描画
+let viewController = FoodTableViewController()
+viewController.title = "テーブルです"
+let navigationController = UINavigationController(rootViewController: viewController)
+navigationController.view.frame = CGRect(x: 0, y: 0, width: 320, height: 480)
+
+//Live Viewに上記のViewを表示させる
+PlaygroundPage.current.liveView = navigationController.view
