ba83e9e45d6ccd28d0d8777dca4c1b86682f8804
tarukawa <shouta859@gmail.com>
2019-09-25 18:36:23 +0900

    storyboard変更

--- Training-iOS/Test2ViewController.swift
+++ Training-iOS/Test2ViewController.swift
@@ -13,22 +13,9 @@ class Test2ViewController: UIViewController {
     @IBAction func nextButton(_ sender: Any) {
         print("pushed")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
