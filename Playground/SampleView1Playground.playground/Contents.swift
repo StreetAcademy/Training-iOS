ab52ed5fdbaecbe10942fba3448185f159ed3deb
fuwamaki <yusaku.maki@street-academy.com>
2019-09-25 12:26:00 +0900

    write tableview

--- Playground/SampleView1Playground.playground/Contents.swift
+++ Playground/SampleView1Playground.playground/Contents.swift
@@ -3,19 +3,43 @@
 import UIKit
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
+let array = ["test1", "test2"]
 
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
-        self.view = view
+final class BeatlesTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
     }
 }
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+
+// Playgroundでの非同期処理を待つオプション
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let viewController = BeatlesTableViewController()
+viewController.title = "I love Beatles."
+let navigationController = UINavigationController(rootViewController: viewController)
+navigationController.view.frame = CGRect(x: 0, y: 0, width: 320, height: 480)
+
+//Live Viewに上記のViewを表示させる
+PlaygroundPage.current.liveView = navigationController.view
+
