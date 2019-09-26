6bcd282ec94acb4c4583a43f0799736c5c4ed602
tarukawa <shouta859@gmail.com>
2019-09-26 14:34:28 +0900

    Json課題5

--- Playground/TestGround_6.playground/Contents.swift
+++ Playground/TestGround_6.playground/Contents.swift
@@ -7,6 +7,7 @@ let json = """
 {
 "total_count": 153139,
 "incomplete_results": false,
+
 "items": [
 {
 "id": 44838949,
@@ -14,9 +15,75 @@ let json = """
 "name": "swift",
 "full_name": "apple/swift",
 "private": false,
+"owner": {
+"login": "apple",
+"id": 10639145,
+"node_id": "MDEyOk9yZ2FuaXphdGlvbjEwNjM5MTQ1",
+"avatar_url": "https://avatars0.githubusercontent.com/u/10639145?v=4",
+"gravatar_id": "",
+"url": "https://api.github.com/users/apple",
+"html_url": "https://github.com/apple",
+"followers_url": "https://api.github.com/users/apple/followers",
+"following_url": "https://api.github.com/users/apple/following{/other_user}",
+"gists_url": "https://api.github.com/users/apple/gists{/gist_id}",
+"starred_url": "https://api.github.com/users/apple/starred{/owner}{/repo}",
+"subscriptions_url": "https://api.github.com/users/apple/subscriptions",
+"organizations_url": "https://api.github.com/users/apple/orgs",
+"repos_url": "https://api.github.com/users/apple/repos",
+"events_url": "https://api.github.com/users/apple/events{/privacy}",
+"received_events_url": "https://api.github.com/users/apple/received_events",
+"type": "Organization",
+"site_admin": false
+},
 "html_url": "https://github.com/apple/swift",
 "description": "The Swift Programming Language",
 "fork": false,
+"url": "https://api.github.com/repos/apple/swift",
+"forks_url": "https://api.github.com/repos/apple/swift/forks",
+"keys_url": "https://api.github.com/repos/apple/swift/keys{/key_id}",
+"collaborators_url": "https://api.github.com/repos/apple/swift/collaborators{/collaborator}",
+"teams_url": "https://api.github.com/repos/apple/swift/teams",
+"hooks_url": "https://api.github.com/repos/apple/swift/hooks",
+"issue_events_url": "https://api.github.com/repos/apple/swift/issues/events{/number}",
+"events_url": "https://api.github.com/repos/apple/swift/events",
+"assignees_url": "https://api.github.com/repos/apple/swift/assignees{/user}",
+"branches_url": "https://api.github.com/repos/apple/swift/branches{/branch}",
+"tags_url": "https://api.github.com/repos/apple/swift/tags",
+"blobs_url": "https://api.github.com/repos/apple/swift/git/blobs{/sha}",
+"git_tags_url": "https://api.github.com/repos/apple/swift/git/tags{/sha}",
+"git_refs_url": "https://api.github.com/repos/apple/swift/git/refs{/sha}",
+"trees_url": "https://api.github.com/repos/apple/swift/git/trees{/sha}",
+"statuses_url": "https://api.github.com/repos/apple/swift/statuses/{sha}",
+"languages_url": "https://api.github.com/repos/apple/swift/languages",
+"stargazers_url": "https://api.github.com/repos/apple/swift/stargazers",
+"contributors_url": "https://api.github.com/repos/apple/swift/contributors",
+"subscribers_url": "https://api.github.com/repos/apple/swift/subscribers",
+"subscription_url": "https://api.github.com/repos/apple/swift/subscription",
+"commits_url": "https://api.github.com/repos/apple/swift/commits{/sha}",
+"git_commits_url": "https://api.github.com/repos/apple/swift/git/commits{/sha}",
+"comments_url": "https://api.github.com/repos/apple/swift/comments{/number}",
+"issue_comment_url": "https://api.github.com/repos/apple/swift/issues/comments{/number}",
+"contents_url": "https://api.github.com/repos/apple/swift/contents/{+path}",
+"compare_url": "https://api.github.com/repos/apple/swift/compare/{base}...{head}",
+"merges_url": "https://api.github.com/repos/apple/swift/merges",
+"archive_url": "https://api.github.com/repos/apple/swift/{archive_format}{/ref}",
+"downloads_url": "https://api.github.com/repos/apple/swift/downloads",
+"issues_url": "https://api.github.com/repos/apple/swift/issues{/number}",
+"pulls_url": "https://api.github.com/repos/apple/swift/pulls{/number}",
+"milestones_url": "https://api.github.com/repos/apple/swift/milestones{/number}",
+"notifications_url": "https://api.github.com/repos/apple/swift/notifications{?since,all,participating}",
+"labels_url": "https://api.github.com/repos/apple/swift/labels{/name}",
+"releases_url": "https://api.github.com/repos/apple/swift/releases{/id}",
+"deployments_url": "https://api.github.com/repos/apple/swift/deployments",
+"created_at": "2015-10-23T21:15:07Z",
+"updated_at": "2019-09-26T04:08:59Z",
+"pushed_at": "2019-09-26T04:29:39Z",
+"git_url": "git://github.com/apple/swift.git",
+"ssh_url": "git@github.com:apple/swift.git",
+"clone_url": "https://github.com/apple/swift.git",
+"svn_url": "https://github.com/apple/swift",
+"homepage": "https://swift.org",
+"size": 475874,
 "stargazers_count": 49217,
 "watchers_count": 49217,
 "language": "C++",
@@ -30,6 +97,13 @@ let json = """
 "archived": false,
 "disabled": false,
 "open_issues_count": 577,
+"license": {
+"key": "apache-2.0",
+"name": "Apache License 2.0",
+"spdx_id": "Apache-2.0",
+"url": "https://api.github.com/licenses/apache-2.0",
+"node_id": "MDc6TGljZW5zZTI="
+},
 "forks": 7937,
 "open_issues": 577,
 "watchers": 49217,
@@ -42,9 +116,74 @@ let json = """
 "name": "swift",
 "full_name": "openstack/swift",
 "private": false,
+"owner": {
+"login": "openstack",
+"id": 324574,
+"node_id": "MDEyOk9yZ2FuaXphdGlvbjMyNDU3NA==",
+"avatar_url": "https://avatars3.githubusercontent.com/u/324574?v=4",
+"gravatar_id": "",
+"url": "https://api.github.com/users/openstack",
+"html_url": "https://github.com/openstack",
+"followers_url": "https://api.github.com/users/openstack/followers",
+"following_url": "https://api.github.com/users/openstack/following{/other_user}",
+"gists_url": "https://api.github.com/users/openstack/gists{/gist_id}",
+"starred_url": "https://api.github.com/users/openstack/starred{/owner}{/repo}",
+"subscriptions_url": "https://api.github.com/users/openstack/subscriptions",
+"organizations_url": "https://api.github.com/users/openstack/orgs",
+"repos_url": "https://api.github.com/users/openstack/repos",
+"events_url": "https://api.github.com/users/openstack/events{/privacy}",
+"received_events_url": "https://api.github.com/users/openstack/received_events",
+"type": "Organization",
+"site_admin": false
+},
 "html_url": "https://github.com/openstack/swift",
 "description": "OpenStack Storage (Swift)",
 "fork": false,
+"url": "https://api.github.com/repos/openstack/swift",
+"forks_url": "https://api.github.com/repos/openstack/swift/forks",
+"keys_url": "https://api.github.com/repos/openstack/swift/keys{/key_id}",
+"collaborators_url": "https://api.github.com/repos/openstack/swift/collaborators{/collaborator}",
+"teams_url": "https://api.github.com/repos/openstack/swift/teams",
+"hooks_url": "https://api.github.com/repos/openstack/swift/hooks",
+"issue_events_url": "https://api.github.com/repos/openstack/swift/issues/events{/number}",
+"events_url": "https://api.github.com/repos/openstack/swift/events",
+"assignees_url": "https://api.github.com/repos/openstack/swift/assignees{/user}",
+"branches_url": "https://api.github.com/repos/openstack/swift/branches{/branch}",
+"tags_url": "https://api.github.com/repos/openstack/swift/tags",
+"blobs_url": "https://api.github.com/repos/openstack/swift/git/blobs{/sha}",
+"git_tags_url": "https://api.github.com/repos/openstack/swift/git/tags{/sha}",
+"git_refs_url": "https://api.github.com/repos/openstack/swift/git/refs{/sha}",
+"trees_url": "https://api.github.com/repos/openstack/swift/git/trees{/sha}",
+"statuses_url": "https://api.github.com/repos/openstack/swift/statuses/{sha}",
+"languages_url": "https://api.github.com/repos/openstack/swift/languages",
+"stargazers_url": "https://api.github.com/repos/openstack/swift/stargazers",
+"contributors_url": "https://api.github.com/repos/openstack/swift/contributors",
+"subscribers_url": "https://api.github.com/repos/openstack/swift/subscribers",
+"subscription_url": "https://api.github.com/repos/openstack/swift/subscription",
+"commits_url": "https://api.github.com/repos/openstack/swift/commits{/sha}",
+"git_commits_url": "https://api.github.com/repos/openstack/swift/git/commits{/sha}",
+"comments_url": "https://api.github.com/repos/openstack/swift/comments{/number}",
+"issue_comment_url": "https://api.github.com/repos/openstack/swift/issues/comments{/number}",
+"contents_url": "https://api.github.com/repos/openstack/swift/contents/{+path}",
+"compare_url": "https://api.github.com/repos/openstack/swift/compare/{base}...{head}",
+"merges_url": "https://api.github.com/repos/openstack/swift/merges",
+"archive_url": "https://api.github.com/repos/openstack/swift/{archive_format}{/ref}",
+"downloads_url": "https://api.github.com/repos/openstack/swift/downloads",
+"issues_url": "https://api.github.com/repos/openstack/swift/issues{/number}",
+"pulls_url": "https://api.github.com/repos/openstack/swift/pulls{/number}",
+"milestones_url": "https://api.github.com/repos/openstack/swift/milestones{/number}",
+"notifications_url": "https://api.github.com/repos/openstack/swift/notifications{?since,all,participating}",
+"labels_url": "https://api.github.com/repos/openstack/swift/labels{/name}",
+"releases_url": "https://api.github.com/repos/openstack/swift/releases{/id}",
+"deployments_url": "https://api.github.com/repos/openstack/swift/deployments",
+"created_at": "2010-07-22T01:50:07Z",
+"updated_at": "2019-09-25T23:12:49Z",
+"pushed_at": "2019-09-25T23:12:47Z",
+"git_url": "git://github.com/openstack/swift.git",
+"ssh_url": "git@github.com:openstack/swift.git",
+"clone_url": "https://github.com/openstack/swift.git",
+"svn_url": "https://github.com/openstack/swift",
+"homepage": "https://opendev.org",
 "size": 108347,
 "stargazers_count": 1952,
 "watchers_count": 1952,
@@ -59,6 +198,13 @@ let json = """
 "archived": false,
 "disabled": false,
 "open_issues_count": 0,
+"license": {
+"key": "apache-2.0",
+"name": "Apache License 2.0",
+"spdx_id": "Apache-2.0",
+"url": "https://api.github.com/licenses/apache-2.0",
+"node_id": "MDc6TGljZW5zZTI="
+},
 "forks": 976,
 "open_issues": 0,
 "watchers": 1952,
@@ -78,40 +224,23 @@ do {
     guard let array = jsonArray else { fatalError() }
     
     // profileの中身は[Key String: Value String]型
-    if let total_count = array["total_count"] as? Int {
-        print(total_count)
-    }
+    //    if let total_count = array["total_count"] as? Int {
+    //        print(total_count)
+    //    }
     
-    // itemsのfull_name取得
     if let items = array["items"] as? [[String: Any]] {
         items.forEach { item in
-            if let fullName = item["full_name"] as? String {
-                print(fullName)
-            }
-        }
-    }
-    
-    // itemsのwatchers_count取得
-    if let items2 = array["items"] as? [[String: Any]] {
-        items2.forEach{ item in
-            if let wathersCount = item["watchers_count"] as? Int {
+            // itemsのfull_name取得
+            // itemsのwatchers_count取得
+            if let fullName = item["full_name"] as? String,
+                let wathersCount = item["watchers_count"] as? Int,
+                // itemsのlanguageを取得
+                let language = item["language"] as? String,
+                // itemsのhas_issueを取得
+                let hasIssues = item["has_issues"] as? Bool {
+                print("fullName：\(fullName)")
                 print("wathersCount： \(wathersCount)")
-            }
-        }
-    }
-    // itemsのlanguageを取得
-    if let items3 = array["items"] as? [[String: Any]] {
-        items3.forEach { item in
-            if let language = item["language"] as? String {
                 print("language：\(language)")
-            }
-        }
-    }
-    
-    // itemsのhas_issueを取得
-    if let items4 = array["items"] as? [[String: Any]] {
-        items4.forEach { item in
-            if let hasIssues = item["has_issues"] as? Bool {
                 print("has_issues：\(hasIssues)")
             }
         }
@@ -121,3 +250,4 @@ do {
 } catch let error {
     print(error)
 }
+
