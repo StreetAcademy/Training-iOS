913845d518770661f921224c0bb3c257472173c4
tarukawa <shouta859@gmail.com>
2019-09-26 10:37:00 +0900

    create Test3.storyboard Test3ViewContlloer.swift

---
+++ Training-iOS/Test3ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Test3ViewController.swift
+//  Training-iOS
+//
+//  Created by Shouta Tarukawa on 2019/09/26.
+//  Copyright © 2019 yusaku maki. All rights reserved.
+//
+
+import UIKit
+
+class Test3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
